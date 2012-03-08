dec64710d311e3c221bea28c214a5687a891ca6c
ivanr <ivanr@atomia.com>
2012-03-08 10:47:33 +0100

    Changed .bat files for GitHubRepository example due change in path for example

--- AutomationServer/Examples/ProvisioningModule/GithubRepo/GitHubRepo/CompileModule.bat
+++ AutomationServer/Examples/ProvisioningModule/GithubRepo/GitHubRepo/CompileModule.bat
@@ -1,3 +1,3 @@
 REM make .exe and rename it
-c:\Python27\Scripts\cxfreeze.bat Atomia.Provisioning.Modules.GitHubRepository\GitHubRepository.py
-copy "Atomia.Provisioning.Modules.GitHubRepository\dist\GitHubRepository.exe" "Atomia.Provisioning.Modules.GitHubRepository\dist\Atomia.Provisioning.Modules.GitHubRepository.exe" /y
+c:\Python27\Scripts\cxfreeze.bat GitHubRepository\GitHubRepository.py
+copy "GitHubRepository\dist\GitHubRepository.exe" "GitHubRepository\dist\Atomia.Provisioning.Modules.GitHubRepository.exe" /y
--- AutomationServer/Examples/ProvisioningModule/GithubRepo/GitHubRepo/InstallModule.bat
+++ AutomationServer/Examples/ProvisioningModule/GithubRepo/GitHubRepo/InstallModule.bat
@@ -1,1 +1,1 @@
-copy "Atomia.Provisioning.Modules.GitHubRepository\dist\*.*" "c:\Program Files (x86)\Atomia\AutomationServer\Common\Modules\GitHubRepository" /y
+copy "GitHubRepository\dist\*.*" "c:\Program Files (x86)\Atomia\AutomationServer\Common\Modules\GitHubRepository" /y
